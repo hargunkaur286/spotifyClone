fea8eedeb2d084ecef1eef26b90dea3513df586d
hargunkaur286 <hargunkaur286@gmail.com>
2022-12-10 03:10:26 +0530

    Added DiscoverPage and Shazam API

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', '4b7f1a6131msh1e357b7d711354bp1960f7jsn82d8c901194a');
+
+            return headers;
+        },
+    }),
+    endpoints: (builder) =>({
+        getTopCharts: builder.query({query: () => '/charts/world'}),
+      
+    }),
+});
+
+export const {
+    useGetTopChartsQuery,
+} = shazamCoreApi;
+
