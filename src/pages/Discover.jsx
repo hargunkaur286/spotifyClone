171050d9f917515d1123aca85875bd39da759953
hargunkaur286 <hargunkaur286@gmail.com>
2022-12-10 19:44:00 +0530

    commit

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -11,7 +11,6 @@ const Discover = () => {
   const { genreListId } = useSelector((state) => state.player);
   const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data, isFetching, error } = useGetSongsByGenreQuery(genreListId || 'POP');
-  const genreTitle = 'Pop';
 
   if (isFetching) return <Loader title="Loading songs..." />;
 
@@ -25,8 +24,8 @@ const Discover = () => {
         <h2 className="font-bold text-3xl text-white text-left">Discover {genreTitle}</h2>
 
         <select
-          onChange={() => {}}
-          value=""
+          onChange={(e) => dispatch(selectGenreListId(e.target.value))}
+          value={genreListId || 'pop'}
           className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
         >
           {genres.map((genre) => <option key={genre.value} value={genre.value}>{genre.title}</option>)}
@@ -38,6 +37,9 @@ const Discover = () => {
           <SongCard
             key={song.key}
             song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
             i={i}
           />
         ))}
